07df2c57cc3097a2baa3e87f657b727c8fea406b
Yudai Sasaki <sasaki@cpnet.co.jp>
2016-07-15 19:17:53 +0900

    　

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -14,7 +14,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 </head>
 <body>
-@yield('container')
+@yield('body')
 @yield('script')
 </body>
 </html>
